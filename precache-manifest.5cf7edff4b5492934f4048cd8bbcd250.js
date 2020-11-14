9d4b0a1f50a224dd1d6b37a93165094812aa4e27
isahasan <esahasan42@gmail.com>
2020-11-14 23:21:57 +0600

    Updates

---
+++ precache-manifest.5cf7edff4b5492934f4048cd8bbcd250.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97636dd67a1f88f4354ea92c53f0e84a",
+    "url": "/worldlocation/index.html"
+  },
+  {
+    "revision": "0c51b68f7e0ee1281b63",
+    "url": "/worldlocation/static/css/main.0ec4e269.chunk.css"
+  },
+  {
+    "revision": "76ebeabe57aa0c6869c9",
+    "url": "/worldlocation/static/js/2.7a546a32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/worldlocation/static/js/2.7a546a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c51b68f7e0ee1281b63",
+    "url": "/worldlocation/static/js/main.d618fc5c.chunk.js"
+  },
+  {
+    "revision": "688d4180bcbc37240681",
+    "url": "/worldlocation/static/js/runtime-main.786188da.js"
+  },
+  {
+    "revision": "c6e741299449015bab5cc0f911ff6b7f",
+    "url": "/worldlocation/static/media/northern.c6e74129.gif"
+  },
+  {
+    "revision": "3b4e5830cdb5c8d154b9bbb8e4bc6d82",
+    "url": "/worldlocation/static/media/southern.3b4e5830.jpg"
+  }
+]);
